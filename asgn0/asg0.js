64bcc20ec0217567aa5938a483e982b01077bc41
Andrew Richmond <41588533+acrmine@users.noreply.github.com>
2026-04-02 13:22:23 -0700

    format changes and link index.html up and working

--- asgn0/asg0.js
+++ asgn0/asg0.js
@@ -7,8 +7,9 @@ function main() {
         return;
     }
     // Get the rendering context for 2DCG <- (2)
-    var ctx = canvas.getRenderingContext('2d');
-    // Draw a blue rectangle <- (3)
-    ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set a blue color
-    ctx.fillRect(120, 10, 150, 150); // Fill a rectangle with the color
+    var ctx = canvas.getContext('2d');
+
+    var v1 = new Vector3([1.0, 2.0, 0.0]);
+
+    
  }
