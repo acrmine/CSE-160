6ffb8004ac614bdc1502266f145d25a1378f06ea
Andrew Richmond <41588533+acrmine@users.noreply.github.com>
2026-04-26 04:54:41 -0700

    Got click rotation working and put in the starts of my blocky animal

--- asgn2/BlockyAnimals.js
+++ asgn2/BlockyAnimals.js
@@ -24,6 +24,16 @@ let u_FragColor
 let u_ModelMatrix
 let u_GlobalRotateMatrix
 
+let g_prevMouse = [0, 0];
+let g_globalAngle = [40, 20];
+let g_globalScale = 80;
+let g_dragSensitivity = 0.7;
+let g_dragging = false;
+
+let g_prevTime = performance.now();
+let g_frameCount = 0;
+let g_fps = 0;
+
 function setupWebGL() {
   // Retrieve <canvas> element
   canvas = document.getElementById('webgl');
@@ -79,7 +89,6 @@ function connectVariablesToGLSL() {
 }
 
 let g_selectedColor = [1.0, 0.0, 0.0, 1.0]; // Default color: red
-let g_globalAngle = [0, 0];
 
 function addActionsForHtmlUI() {
   // Slider events (Shape color)
@@ -102,22 +111,45 @@ function main() {
   addActionsForHtmlUI();
 
   // Register function (event handler) to be called on a mouse move while left click is pressed
-  //canvas.onmousedown = click;
-  canvas.addEventListener("mousemove", (ev)=> {
-    if(ev.buttons == 1) {
-      g_globalAngle = [ev.offsetX, ev.offsetY];
-      renderAllShapes();
-    }
-  });
+  canvas.onmousedown = click;
+  canvas.onmousemove = mouseMove;
+  canvas.onmouseup = mouseUp;
+  canvas.onmouseleave = exitCanvas;
+  canvas.onwheel = scroll;
 
   // Specify the color for clearing <canvas>
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearColor(0.0, 0.0, 0.0, 0.5);
 
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   renderAllShapes();
+
+  tick();
 }
+
+function tick() {
+  requestAnimationFrame(tick);
+  renderAllShapes();
+}
+
+function updateFPS(currTime) {
+  g_frameCount++;
+
+  const delta = currTime - g_prevTime;
+  if (delta >= 1000) {
+    g_fps = (g_frameCount * 1000) / delta;
+
+    g_frameCount = 0;
+    g_prevTime = currTime;
+
+    document.getElementById('fps').textContent = `FPS: ${g_fps.toFixed(2)}`;
+  }
+
+  requestAnimationFrame(updateFPS);
+}
+
+requestAnimationFrame(updateFPS);
 
 // Extract the event click and convert it to WebGL coordinates
 function convertCoordinatesEventToGL(ev) {
@@ -131,25 +163,65 @@ function convertCoordinatesEventToGL(ev) {
   return ([x, y]);
 }
 
+// Click detection setup for rotating camera inspired by Milo Kesteloot's setup in Ender Dragon
+// https://milokesteloot.github.io/MinecraftEntities/
+
+function click(ev) {
+  g_dragging = true;
+  g_prevMouse[0] = ev.clientX;
+  g_prevMouse[1] = ev.clientY;
+}
+
+function mouseMove(ev) {
+  if (!g_dragging) {
+    return;
+  }
+  let deltaX = ev.clientX - g_prevMouse[0];
+  let deltaY = ev.clientY - g_prevMouse[1];
+  g_globalAngle[0] += deltaX * g_dragSensitivity;
+  g_globalAngle[1] += deltaY * g_dragSensitivity;
+  g_prevMouse[0] = ev.clientX;
+  g_prevMouse[1] = ev.clientY;
+}
+
+function mouseUp(ev) {
+  g_dragging = false;
+}
+
+function exitCanvas(ev) {
+  g_dragging = false;
+}
+
+function scroll(ev) {
+  const scroll = ev.deltaY;
+  g_globalScale *= (-scroll) * 0.001 + 1;
+  ev.preventDefault();
+}
+
 // Draw every shape that is supposed to be in the canvas
 function renderAllShapes() {
-  var globalRotMat = new Matrix4().rotate(0, 0, 1, 0);
+  let scale = 1/160 * g_globalScale;
+  let globalRotMat = new Matrix4()
+    .scale(scale, scale, scale)
+    .rotate(g_globalAngle[0], 0, -1, 0)
+    .rotate(g_globalAngle[1], -1, 0, 0);
   gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
 
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  // Draw a cube
-  var body = new Cube();
-  body.color = [1.0,0.0,0.0,1.0];
-  body.matrix.setTranslate(-0.25, -0.5, 0.0);
-  body.matrix.scale(0.5, 1.0, 0.5);
+  // main body
+  var body = new Cube([1, 1, 1, 1]);
+  body.matrix.scale(0.8, 1.0, 0.6);
   body.render();
 
-  var leftArm = new Cube();
-  leftArm.color = [1,1,0,1];
-  leftArm.matrix.setTranslate(0.7, 0.0, 0.0);
-  leftArm.matrix.rotate(45, 0, 0, 1);
-  leftArm.matrix.scale(0.25, 0.7, 0.5);
-  leftArm.render();
+  var neck1 = new Cube([1,1,1,1]);
+  neck1.matrix.setTranslate(0, 0.6, 0.0);
+  neck1.matrix.scale(0.6, 0.2, 0.5);
+  neck1.render();
+
+  var hips = new Cube([1,1,1,1]);
+  neck1.matrix.setTranslate(0, -0.54, 0.0);
+  neck1.matrix.scale(0.7, 0.1, 0.5);
+  neck1.render();
 }
--- asgn2/Cube.js
+++ asgn2/Cube.js
@@ -1,7 +1,7 @@
 class Cube {
-  constructor() {
+  constructor(color = [1.0, 1.0, 1.0, 1.0]) {
     this.type = 'cube';
-    this.color = [1.0, 1.0, 1.0, 1.0];
+    this.color = color;
 
     this.matrix = new Matrix4();
   }
@@ -15,31 +15,31 @@ class Cube {
     gl.uniformMatrix4fv(u_ModelMatrix, false, this.matrix.elements);
 
     // Front of cube
-    drawTriangle3D([0,0,0, 1,1,0,  1,0,0]);
-    drawTriangle3D([0,0,0, 0,1,0,  1,1,0]);
+    drawTriangle3D([-0.5,-0.5,-0.5, 0.5,0.5,-0.5,  0.5,-0.5,-0.5]);
+    drawTriangle3D([-0.5,-0.5,-0.5, -0.5,0.5,-0.5,  0.5,0.5,-0.5]);
 
     // Back of cube
-    drawTriangle3D([0,0,1, 1,0,1, 1,1,1]);
-    drawTriangle3D([0,0,1, 1,1,1, 0,1,1]);
+    drawTriangle3D([-0.5,-0.5,0.5, 0.5,-0.5,0.5, 0.5,0.5,0.5]);
+    drawTriangle3D([-0.5,-0.5,0.5, 0.5,0.5,0.5, -0.5,0.5,0.5]);
 
     gl.uniform4f(u_FragColor, rgba[0] * 0.9, rgba[1] * 0.9, rgba[2] * 0.9, rgba[3]);
 
     // Top of cube
-    drawTriangle3D([0,1,0, 0,1,1, 1,1,1]);
-    drawTriangle3D([0,1,0, 1,1,1, 1,1,0]);
+    drawTriangle3D([-0.5,0.5,-0.5, -0.5,0.5,0.5, 0.5,0.5,0.5]);
+    drawTriangle3D([-0.5,0.5,-0.5, 0.5,0.5,0.5, 0.5,0.5,-0.5]);
 
     // Bottom of cube
-    drawTriangle3D([0,0,0, 1,0,0, 1,0,1]);
-    drawTriangle3D([0,0,0, 1,0,1, 0,0,1]);
+    drawTriangle3D([-0.5,-0.5,-0.5, 0.5,-0.5,-0.5, 0.5,-0.5,0.5]);
+    drawTriangle3D([-0.5,-0.5,-0.5, 0.5,-0.5,0.5, -0.5,-0.5,0.5]);
 
     gl.uniform4f(u_FragColor, rgba[0] * 0.8, rgba[1] * 0.8, rgba[2] * 0.8, rgba[3]);
 
     // Left of cube
-    drawTriangle3D([0,0,0, 0,1,0, 0,1,1]);
-    drawTriangle3D([0,0,0, 0,1,1, 0,0,1]);
+    drawTriangle3D([-0.5,-0.5,-0.5, -0.5,0.5,-0.5, -0.5,0.5,0.5]);
+    drawTriangle3D([-0.5,-0.5,-0.5, -0.5,0.5,0.5, -0.5,-0.5,0.5]);
 
     // Right of cube
-    drawTriangle3D([1,0,0, 1,1,0, 1,1,1]);
-    drawTriangle3D([1,0,0, 1,1,1, 1,0,1]);
+    drawTriangle3D([0.5,-0.5,-0.5, 0.5,0.5,-0.5, 0.5,0.5,0.5]);
+    drawTriangle3D([0.5,-0.5,-0.5, 0.5,0.5,0.5, 0.5,-0.5,0.5]);
   }
 }
