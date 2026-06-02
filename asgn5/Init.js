f5a6557a02d126a606132464dd73aa5ef7f8a2f5
Andrew Richmond <41588533+acrmine@users.noreply.github.com>
2026-06-01 20:16:05 -0700

    Trying to fit the module script file format

--- asgn5/Init.js
+++ asgn5/Init.js
@@ -5,26 +5,26 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 const fixedTimeStep = 1.0 / 60.0;
 const maxSubSteps = 3;
 
-var g_keys = {};
+export var g_keys = {};
 
-var scene = new THREE.Scene();
+export var scene = new THREE.Scene();
 scene.background = new THREE.Color(0xaaaaaa);
 
-var phys_world = new CANNON.World();
+export var phys_world = new CANNON.World();
 phys_world.gravity.set(0, 0, -9.82);
 
-var renderer = new THREE.WebGLRenderer();
+export var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var g_prevTime = performance.now();
-var g_frameCount = 0;
-var g_fps = 0;
-var g_frameDelta = 1.0 / 60.0;
+export var g_prevTime = performance.now();
+export var g_frameCount = 0;
+export var g_fps = 0;
+export var g_frameDelta = 1.0 / 60.0;
 
-var g_dynamic_objects = [];
+export var g_dynamic_objects = [];
 
-const approachZero = (num, delta) => {
+export const approachZero = (num, delta) => {
   if (num > 0) {
     return Math.max(0, num - delta);
   }
